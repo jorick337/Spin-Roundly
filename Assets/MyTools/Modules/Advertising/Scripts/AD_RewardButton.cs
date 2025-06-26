52187bb6128b4d6ea9fcb5654c979a2a35a75d79
jorick337 <jorick337@gmail.com>
2025-06-26 08:02:18 +0300

    Advertising: remade script AD_RewardButton

--- Assets/MyTools/Modules/Advertising/Scripts/AD_RewardButton.cs
+++ Assets/MyTools/Modules/Advertising/Scripts/AD_RewardButton.cs
@@ -7,20 +7,31 @@ namespace MyTools.Advertising
     public class AD_RewardButton : MonoBehaviour
     {
         [SerializeField] private Button _button;
+        [SerializeField] private Text _text;
         [SerializeField] private int _reward;
 
         // Managers
         private PlayerManager _playerManager;
 
-        private void Awake() => _playerManager = PlayerManager.Instance;
+        private void Awake() => Initialize();
         private void OnEnable() => _button.onClick.AddListener(Click);
         private void OnDisable() => _button.onClick.RemoveListener(Click);
 
-        private void Click()
+        private void Initialize()
         {
-            Reward();
+            _playerManager = PlayerManager.Instance;
+            SetReward();
         }
 
-        private void Reward() => _playerManager.Player.AddMoney(_reward);
+        private void Click()
+        {
+            GiveReward();
+        }
+
+        private void GiveReward() => _playerManager.Player.AddMoney(_reward);
+
+
+        private void SetReward() => SetText($"+{_reward}");
+        private void SetText(string text) => _text.text =  text;
     }
 }
