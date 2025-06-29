6061268a75d726f67230104844c91e62e6b14c77
jorick337 <jorick337@gmail.com>
2025-06-29 09:32:02 +0300

    Levels: add Level12; fix object Square -> Stone which did`nt restart before; add script Stone for its

---
+++ Assets/MyTools/Modules/Levels/Scripts/2D/Objects/LevelItems/Stone_ST2.cs
@@ -0,0 +1,17 @@
+using MyTools.UI;
+using UnityEngine;
+
+namespace MyTools.Levels.TwoDimensional.Objects
+{
+    public class Stone_ST2 : LevelItem
+    {
+        [Header("Stone")]
+        [SerializeField] private Teleport _teleport;
+
+        protected override void DoActionBeforeRestart() => _teleport.SendToTarget();
+        protected override void DoActionOnAwake() { }
+        protected override void InvokeTriggeredEnter(Collider2D collider2D) { }
+        protected override void InvokeTriggeredExit(Collider2D collider2D) { }
+        protected override void InvokeTriggeredStay(Collider2D collider2D) { }
+    }
+}
