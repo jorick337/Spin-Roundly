283897cca6c5206dda9d062480747734e75fa20d
jorick337 <jorick337@gmail.com>
2025-07-10 10:56:27 +0300

    Remove script PL2_Effects; Remade script EffectColliderTrigger; Add material of Dirt And Snow

--- Assets/MyTools/Tools/UI/Scripts/Objects/Colliders/EffectColliderTrigger.cs
+++ Assets/MyTools/Tools/UI/Scripts/Objects/Colliders/EffectColliderTrigger.cs
@@ -5,10 +5,20 @@ namespace MyTools.UI.Colliders
     public class EffectColliderTrigger : MonoBehaviour
     {
         [SerializeField] private ColliderTrigger2D _colliderTrigger2D;
+        [SerializeField] private ParticleSystem _particleSystem;
         [SerializeField] private AudioSource _audioSource;
 
-        private void OnEnable() => _colliderTrigger2D.OnTriggeredEnter += PlayAudioSource;
-        private void OnDisable() => _colliderTrigger2D.OnTriggeredEnter -= PlayAudioSource;
+        private void OnEnable()
+        {
+            _colliderTrigger2D.OnTriggeredEnter += Enter;
+            _colliderTrigger2D.OnTriggeredExit += Exit;
+        }
+
+        private void OnDisable()
+        {
+            _colliderTrigger2D.OnTriggeredEnter -= Enter;
+            _colliderTrigger2D.OnTriggeredExit -= Exit;
+        }
 
         private void OnValidate()
         {
@@ -16,8 +26,26 @@ namespace MyTools.UI.Colliders
                 _colliderTrigger2D = GetComponent<ColliderTrigger2D>();
             if (_audioSource == null)
                 _audioSource = GetComponent<AudioSource>();
+            if (_particleSystem == null)
+                _particleSystem = GetComponent<ParticleSystem>();
         }
 
-        private void PlayAudioSource(Collider2D collider2D) => _audioSource?.Play();
+        private void Enter(Collider2D collider)
+        {
+            if (_particleSystem != null)
+                _particleSystem.Play();
+
+            if (_audioSource != null)
+                _audioSource.Play();
+        }
+
+        private void Exit(Collider2D collider)
+        {
+            if (_particleSystem != null)
+                _particleSystem.Stop();
+
+            if (_audioSource != null)
+                _audioSource.Stop();
+        }
     }
 }
